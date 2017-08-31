2ecd91bfc47dffa5997a2f2c92ee6eec9fe13e3e
John Li <dhzhd1@gmail.com>
2017-08-30 23:28:46 -0700

    Add new image dataset

--- lib/dataset/__init__.py
+++ lib/dataset/__init__.py
@@ -1,4 +1,5 @@
 from imdb import IMDB
-from pascal_voc import PascalVOC
-from cityscape import CityScape
-from coco import coco
+# from pascal_voc import PascalVOC
+# from cityscape import CityScape
+# from coco import coco
+from road_images import RoadImages
