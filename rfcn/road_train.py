486b182a291e0a998aa091f60010b5c1a95d4f42
dhzhd1 <dhzhd1@gmail.com>
2017-09-01 16:36:46 -0700

    Update training code

--- rfcn/road_train.py
+++ rfcn/road_train.py
@@ -146,7 +146,7 @@ def train_net(args, ctx, pretrained, epoch, prefix, begin_epoch, end_epoch, lr, 
     mod.fit(train_data, eval_metric=eval_metrics, epoch_end_callback=epoch_end_callback,
             batch_end_callback=batch_end_callback, kvstore=config.default.kvstore,
             optimizer='sgd', optimizer_params=optimizer_params,
-            arg_params=arg_params, aux_params=aux_params, begin_epoch=begin_epoch, num_epoch=end_epoch)
+            arg_params=arg_params, aux_params=aux_params, begin_epoch=begin_epoch, num_epoch=end_epoch, allow_extra=False)
 
 def main():
     print('Called with argument:', args)
