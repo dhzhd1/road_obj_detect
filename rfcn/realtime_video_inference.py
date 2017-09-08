dc751bdfca84bee10a8b3836046b0505fea7caee
John Li <dhzhd1@gmail.com>
2017-09-07 17:46:58 -0700

    modify code

--- rfcn/realtime_video_inference.py
+++ rfcn/realtime_video_inference.py
@@ -43,9 +43,7 @@ def main(video_file):
     classes = ['vehicle', 'pedestrian', 'cyclist', 'traffic lights']
 
     cap = cv2.VideoCapture(video_path)
-    count = 1
     while (cap.isOpened()):
-        print(count)
         ret, frame = cap.read()
         tic()
         data = []
@@ -60,13 +58,13 @@ def main(video_file):
         data_names = ['data', 'im_info']
         label_names = []
         data = [[mx.nd.array(data[i][name]) for name in data_names] for i in xrange(len(data))]
-        print('Debug: [data] shape: {}, cont: {}'.format(type(data), data))
+        # print('Debug: [data] shape: {}, cont: {}'.format(type(data), data))
         max_data_shape = [[('data', (1, 3, max([v[0] for v in config.SCALES]), max([v[1] for v in config.SCALES])))]]
-        print('Debug: [max_data_shape] shape: {}, cont: {}'.format(type(max_data_shape), max_data_shape))
+        # print('Debug: [max_data_shape] shape: {}, cont: {}'.format(type(max_data_shape), max_data_shape))
         provide_data = [[(k, v.shape) for k, v in zip(data_names, data[i])] for i in xrange(len(data))]
-        print('Debug: [provide_data] shape: {}, cont: {}'.format(type(provide_data), provide_data))
+        # print('Debug: [provide_data] shape: {}, cont: {}'.format(type(provide_data), provide_data))
         provide_label = [None for i in xrange(len(data))]
-        print('Debug: [provide_label] shape: {}, cont: {}'.format(type(provide_label), provide_label))
+        # print('Debug: [provide_label] shape: {}, cont: {}'.format(type(provide_label), provide_label))
         predictor = Predictor(sym, data_names, label_names,
                               context=[mx.gpu(0)], max_data_shapes=max_data_shape,
                               provide_data=provide_data, provide_label=provide_label,
@@ -80,7 +78,7 @@ def main(video_file):
                                          provide_data=[[(k, v.shape) for k, v in zip(data_names, data[idx])]],
                                          provide_label=[None])
             scales = [data_batch.data[i][1].asnumpy()[0, 2] for i in xrange(len(data_batch.data))]
-            print('Debug: [scales] cont: {}'.format(scales))
+            # print('Debug: [scales] cont: {}'.format(scales))
 
             scores, boxes, data_dict = im_detect(predictor, data_batch, data_names, scales, config)
             boxes = boxes[0].astype('f')
@@ -121,10 +119,10 @@ def draw_bbox_on_frame(frame, dets_nms, classes, scale=1.0):
                             cv2.FONT_HERSHEY_PLAIN,
                             0.5,
                             color)
-                print('Bbox: {}, Class: {}, Prob: {}'.format(bbox, cls_name, score))
+                # print('Bbox: {}, Class: {}, Prob: {}'.format(bbox, cls_name, score))
     return frame
 
 
 if __name__ == '__main__':
-    video_path = '../../video/Downtown.mp4'
+    video_path = '../../video/Highway.mp4'
     main(video_path)
